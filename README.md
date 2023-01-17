174f85e57e55f9aef00735098b1180133ea58733
Justin Zandbergen <justin.zandbergen@topicus.nl>
2023-01-17 22:23:15 +0100

    It compiles, ship it!

---
+++ README.md
@@ -0,0 +1,45 @@
+# about
+
+`promfmt` is a very naive PromQL formatter. One can pipe a promql query in it, and
+it gets formatted.
+
+
+I really hope someone will create a proper formatter some day, but for now this
+one will have to do in my vimrc :).
+
+## Example
+
+```
+echo 'sum by (bla, bladiebla, woot) (rate(somemetric{labelx="asdfasdf",labely="jajanounou"}[5m])) * 1024' | ./promfmt
+
+sum by (bla, bladiebla, woot) (
+    rate(
+      somemetric{labelx="asdfasdf",labely="jajanounou"}[5m]
+    )
+  )
+*
+  1024
+```
+
+### or in `.vimrc`
+```vimrc
+augroup promql
+  autocmd!
+  autocmd FileType promql nmap <F3> :%!cat - \| promfmt<CR>
+augroup END 
+```
+
+## Building
+
+```
+go build
+```
+
+## Copyright
+
+The parser code is shamelessly copied from the prometheus source code which
+is licensed under the apache license. I've only copied it because I needed
+to access an unexported variable.
+
+
+
---
+++ main.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+	"strings"
+
+	"github.com/jzandbergen/promfmt/parser"
+)
+
+func main() {
+	fi, _ := os.Stdin.Stat()
+
+	if (fi.Mode() & os.ModeCharDevice) == 0 {
+		fmt.Println("data is from pipe")
+	} else {
+		fmt.Println("data is from terminal")
+	}
+
+	// we read from stdin
+	reader := bufio.NewReader(os.Stdin)
+	buf := make([]byte, 0, 4*1024)
+
+	var s strings.Builder
+
+	for {
+		// read max CAPacity of buffer
+		n, err := reader.Read(buf[:cap(buf)])
+		// if data is smaller than buffer, trim data.
+		buf = buf[:n]
+
+		// no more data to read?
+		if n == 0 {
+			if err == nil {
+				// nothing read, try again...
+				continue
+			}
+			// error handing
+			if err == io.EOF {
+				break
+			}
+			fmt.Println(err)
+			os.Exit(1)
+		}
+
+		// process buffer
+		s.Write(buf)
+	}
+
+	// Thank you so much peoples of the prometheus for already providing
+	// a pretty printer. The only reason I stole your parser code is because
+	// the maxCharactersPerLine was not exported and I want the pretty code
+	// to break the line earlier than 100 chars.
+	parser.MaxCharactersPerLine = 10
+
+	x, err := parser.ParseExpr(s.String())
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	y := parser.Prettify(x)
+	fmt.Println(y)
+}
